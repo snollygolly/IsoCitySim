8618ef8bdc673516d76fe93f2197282aedc9342f
SLoW <snollygolly@gmail.com>
2015-04-03 21:41:33 -0500

    lazy loading of chunks is functional. yassss

--- game/plugins/worldmanager.js
+++ game/plugins/worldmanager.js
@@ -161,19 +161,18 @@ WorldManager.prototype = {
           if (l == 0 || l == 1 || l == 2){
             if (i == 0){
               //set top of chunk
-              this.chunks[c].top = tile.y;
+              this.chunks[c].top = tile.y - this.world.tile_size;
             }
             else if (i == (this.world.units - 1)){
               //set right of chunk
-              this.chunks[c].right = tile.x;
+              this.chunks[c].right = tile.x - (this.world.tile_size / 2);
             }
             else if (i == (this.world.units * (this.world.units - 1))){
               //set left of chunk
-              this.chunks[c].left = tile.x;
+              this.chunks[c].left = tile.x + (this.world.tile_size / 2);
             }
             else if (i == (this.world.units * this.world.units) - 1){
               //set bottom of chunk
-              console.log("setting chunk: " + c + " bottom to: " + tile.y);
               this.chunks[c].bottom = tile.y;
             }
           }
--- game/states/boot.js
+++ game/states/boot.js
@@ -4,6 +4,7 @@
 var game;
 var cursors;
 var rS;
+var wPx, hPx;
 
 function Boot() {
   rS = new rStats( {
@@ -19,11 +20,9 @@ Boot.prototype = {
   preload: function() {
     game = this.game;
     //generate the world
-    var wPx = game.worldManager.world.chunks * (game.worldManager.world.units * 132);
-    var hPx = game.worldManager.world.chunks * (game.worldManager.world.units * 74);
+    wPx = game.worldManager.world.chunks * (game.worldManager.world.units * 132);
+    hPx = game.worldManager.world.chunks * (game.worldManager.world.units * 74);
     game.world.setBounds(0, 0, wPx, hPx);
-    game.camera.x = (wPx / 2) - (1024 / 2);
-    game.camera.y = (hPx / 2) - (768 / 2);
     //generate all the layers
     game.worldManager.createWorld(game.generate.generateMap(game.worldManager.world, 0));
     //build the chunk!
@@ -44,11 +43,12 @@ Boot.prototype = {
     //set up plugins and game
     game.plugins.add(new Phaser.Plugin.Isometric(game));
     game.physics.startSystem(Phaser.Plugin.Isometric.ISOARCADE);
-    game.iso.anchor.setTo(0.5, 0.2);
+    game.iso.anchor.setTo(0.5, 0.1);
   },
   create: function() {
     game.worldManager.drawWorld();
     cursors = game.input.keyboard.createCursorKeys();
+    this.moveCamera((wPx / 2) - (1024 / 2), (hPx / 2) - (768 / 2));
   },
   update: function () {
     rS( 'FPS' ).frame();
@@ -58,17 +58,16 @@ Boot.prototype = {
     //this is how scaling is done, but this code is super rough
     //isoGroup.scale.setTo(2,2);
     if (cursors.right.isDown){
-      this.moveCamera(0,0);
-      game.camera.x += 10;
+      this.moveCamera((game.world.camera.x + 10), game.world.camera.y);
     }
     if (cursors.left.isDown){
-      game.camera.x -= 10;
+      this.moveCamera((game.world.camera.x - 10), game.world.camera.y);
     }
     if (cursors.down.isDown){
-      game.camera.y += 10;
+      this.moveCamera(game.world.camera.x, (game.world.camera.y + 10));
     }
     if (cursors.up.isDown){
-      game.camera.y -= 10;
+      this.moveCamera(game.world.camera.x, (game.world.camera.y - 10));
     }
     rS( 'update' ).end();
   },
@@ -79,54 +78,35 @@ Boot.prototype = {
     rS().update();
   },
   moveCamera: function(x, y){
-    //var isoCam = game.iso.unproject(game.world.camera.view);
+    game.world.camera.setPosition(x,y);
     var isoCam = game.world.camera.view;
-    var isoChunk = game.worldManager.chunks[0];
-    //console.log("camera: x: " + isoCam.x + " y: " + isoCam.y);
-    //console.log("chunk: t: " + isoChunk.top + " l: " + isoChunk.left);
+
     var viewport = {
       left: isoCam.x,
       right: isoCam.x + 1024,
       top: isoCam.y,
       bottom: isoCam.y + 768
     };
-    // var chunkBounds = game.worldManager.chunks[0].group.getBounds();
-    // var isoChunk = game.iso.unproject(game.worldManager.chunks[0].group);
-    // var chunk = {
-    //   left: isoChunk.x,
-    //   right: isoChunk.x + chunkBounds.width,
-    //   top: isoChunk.y,
-    //   bottom: isoChunk.y + chunkBounds.height
-    // };
-    if (intersectRect(isoChunk, viewport) === true){
-      //game.worldManager.chunks[i].group.visible = true;
-      console.log("chunk : " + 0 + " is visible");
-    }else{
-      //game.worldManager.chunks[i].group.visible = false;
-      console.log("chunk : " + 0 + " is invisible");
-    }
-    return;
+
     var i = 0;
     while (i < game.worldManager.chunks.length){
-      //if (intersectRect(game.worldManager.chunks[i].group.getBounds(), game.world.camera.screenView) === true){
-      if (intersectRect(game.worldManager.chunks[i].group.getBounds(), game.world.camera.view) === true){
+      if (intersectRect(game.worldManager.chunks[i], viewport) === true){
         //do something
         game.worldManager.chunks[i].group.visible = true;
-        console.log("chunk : " + i + " is visible");
-        //console.log(game.worldManager.chunks[i]);
+        //console.log("chunk : " + i + " is visible");
       }else{
         game.worldManager.chunks[i].group.visible = false;
-        console.log("chunk : " + i + " is invisible");
+        //console.log("chunk : " + i + " is invisible");
       }
       i++;
     }
     //functions
     function intersectRect(r1, r2) {
-      console.log("intersect: (chunk / camera)");
-      console.log("camera.left (" + r2.left + ") > chunk.right (" + r1.right + ") - [" + ((r2.left > r1.right)?"bad":"good") + "]");
-      console.log("camera.right (" + r2.right + ") > chunk.left (" + r1.left + ") - [" + ((r2.right < r1.left)?"bad":"good") + "]");
-      console.log("camera.top (" + r2.top + ") > chunk.bottom (" + r1.bottom + ") - [" + ((r2.top > r1.bottom)?"bad":"good") + "]");
-      console.log("camera.bottom (" + r2.bottom + ") > chunk.top (" + r1.top + ") - [" + ((r2.bottom < r1.top)?"bad":"good") + "]");
+      // console.log("intersect: (chunk / camera)");
+      // console.log("camera.left (" + r2.left + ") > chunk.right (" + r1.right + ") - [" + ((r2.left > r1.right)?"bad":"good") + "]");
+      // console.log("camera.right (" + r2.right + ") > chunk.left (" + r1.left + ") - [" + ((r2.right < r1.left)?"bad":"good") + "]");
+      // console.log("camera.top (" + r2.top + ") > chunk.bottom (" + r1.bottom + ") - [" + ((r2.top > r1.bottom)?"bad":"good") + "]");
+      // console.log("camera.bottom (" + r2.bottom + ") > chunk.top (" + r1.top + ") - [" + ((r2.bottom < r1.top)?"bad":"good") + "]");
       return !(r2.left > r1.right ||
                r2.right < r1.left ||
                r2.top > r1.bottom ||
